9aa7e67d1b0aa515284aa0fa9a3ea18b6ab480b2
carolineoliveiranunes606-cloud <carolineoliveiranunes606@gmail.com>
2026-05-19 12:17:07 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from "express";
-import createApp from "./app";
+import { buildApp } from "./app";
 
-const app = createApp();
-const port = process.env.PORT;
+const app = buildApp();
 
-app.listen(port, () => {
-  console.log(`🔥 Server running at port http://localhost:${port}`);
+const PORT = Number(process.env.PORT) || 3333;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`
+  ====================================
+   🚀 Champions API running
+   🌍 http://localhost:${PORT}
+  ====================================
+  `);
 });
