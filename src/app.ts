76431fbe1004d123e175a70b84b91c04dd2709f9
carolineoliveiranunes606-cloud <carolineoliveiranunes606@gmail.com>
2026-05-19 12:16:49 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,39 @@
 import express from "express";
-import router from "./routes";
 import cors from "cors";
+import router from "./routes";
 
-function createApp() {
+export function buildApp() {
   const app = express();
 
+  app.disable("x-powered-by");
+
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use("/api", router);
 
-  const corsOptions = {
-    origin: ["http://felipao.sistem.com", "http://gov.br"],
-    methods: ["GET", "UPDATE"],
-  };
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
 
-  app.use(cors());
+  app.use((request, _, next) => {
+    console.log(
+      `[${new Date().toISOString()}] ${request.method} ${request.url}`
+    );
+
+    next();
+  });
+
+  app.get("/", (_, response) => {
+    return response.status(200).json({
+      project: "Champions League API",
+      version: "2.0.0",
+      status: "online",
+    });
+  });
+
+  app.use("/api/v1", router);
 
   return app;
 }
-
-export default createApp;
